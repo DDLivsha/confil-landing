6304aaa8eaa8d2c200c3c81f01181225f8a68539
Livsha <ddlivsha@gmail.com>
2025-07-29 17:25:51 +0300

    how it works mobile done

--- components/how-it-works/HowItWorks.tsx
+++ components/how-it-works/HowItWorks.tsx
@@ -23,7 +23,7 @@ const HowItWorks: FC<Props> = ({ className }) => {
          <div className=''>
             <div className='container explain__card --reverse'>
                <Image src={first} alt="Create or Upload a Form" width={520} height={348} className='explain__image' />
-               <div className='flex flex-col gap-1.5 max-w-[450px]'>
+               <div className='flex flex-col gap-1.5'>
                   <h3 className="explain__title --blue">1. Create or Upload a Form</h3>
                   <p className="explain__text">Start with a blank digital form, choose from ready-made templates, upload a PDF or scan paper documents directly into Confil.</p>
                </div>
@@ -32,7 +32,7 @@ const HowItWorks: FC<Props> = ({ className }) => {
          <div className='relative'>
             <div className='container explain__card '>
                <Image src={second} alt="Customize Fields & Design" width={520} height={348} className='explain__image' />
-               <div className='flex flex-col gap-1.5 max-w-[450px]'>
+               <div className='flex flex-col gap-1.5'>
                   <h3 className="explain__title">2. Customize Fields & Design</h3>
                   <p className="explain__text --white">Drag and drop text fields, checkboxes, signatures, and other elements. Adjust the layout and styling to match your needs and branding.</p>
                </div>
@@ -43,7 +43,7 @@ const HowItWorks: FC<Props> = ({ className }) => {
          <div className=''>
             <div className='container explain__card --reverse'>
                <Image src={third} alt="Autofill or Fill Manually" width={520} height={348} className='explain__image' />
-               <div className='flex flex-col gap-1.5 max-w-[450px]'>
+               <div className='flex flex-col gap-1.5'>
                   <h3 className="explain__title --blue">3. Autofill or Fill Manually</h3>
                   <p className="explain__text">Use smart autofill to populate known data instantly or fill out fields manually for full control.</p>
                </div>
@@ -53,7 +53,7 @@ const HowItWorks: FC<Props> = ({ className }) => {
          <div className=''>
             <div className='container explain__card'>
                <Image src={fourth} alt="Send to Patients or Save as Draft" width={520} height={348} className='explain__image' />
-               <div className='flex flex-col gap-1.5 max-w-[450px]'>
+               <div className='flex flex-col gap-1.5'>
                   <h3 className="explain__title --blue">4. Send to Patients or Save as Draft</h3>
                   <p className="explain__text">Share the form securely with patients for completion and signing, or save it internally for future use. Mark favorites for quick access.</p>
                </div>
@@ -63,7 +63,7 @@ const HowItWorks: FC<Props> = ({ className }) => {
          <div className='relative'>
             <div className='container explain__card --reverse'>
                <Image src={fifth} alt="Collect Signatures & Track Progress" width={520} height={348} className='explain__image' />
-               <div className='flex flex-col gap-1.5 max-w-[450px]'>
+               <div className='flex flex-col gap-1.5'>
                   <h3 className="explain__title">5. Collect Signatures & Track Progress</h3>
                   <p className="explain__text --white">Receive secure digital signatures and monitor signing status in real time.</p>
                </div>
@@ -74,7 +74,7 @@ const HowItWorks: FC<Props> = ({ className }) => {
          <div className=''>
             <div className='container explain__card'>
                <Image src={sixth} alt="Archive, Export, or Print" width={520} height={348} className='explain__image' />
-               <div className='flex flex-col gap-1.5 max-w-[450px]'>
+               <div className='flex flex-col gap-1.5'>
                   <h3 className="explain__title --blue">6. Archive, Export, or Print</h3>
                   <p className="explain__text">Archive signed forms, export as PDF, or print copies when needed. All documents are safely stored and easy to manage.</p>
                </div>
