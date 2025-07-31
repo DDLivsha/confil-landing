069d3e7216f68a28a3673ffea594f9a8b5968569
Livsha <ddlivsha@gmail.com>
2025-07-31 18:21:26 +0300

    trim

--- helpers/contact-form-zod-schema.ts
+++ helpers/contact-form-zod-schema.ts
@@ -2,12 +2,12 @@ import { error } from 'console'
 import { z } from 'zod'
 
 export const contactFormSchema = z.object({
-   name: z.string().min(2, { error: 'You need to fill this field' }).max(80, { error: 'Maximum 80 characters' }).regex(
+   name: z.string().trim().min(2, { error: 'You need to fill this field' }).max(80, { error: 'Maximum 80 characters' }).regex(
          /^(?!.* {2})[a-zA-Zа-яА-ЯіїєґІЇЄҐ'-]+(?: [a-zA-Zа-яА-ЯіїєґІЇЄҐ'-]+)*$/,
          { error: 'Only letters' }
       ),
    email: z.email({ error: 'Invalid email' }).min(2, { error: 'You need to fill this field' }).max(80, { error: 'Maximum 80 characters' }),
-   message: z.string().min(10, { error: 'Minimum 10 characters' }).max(400, { error: 'Maximum 400 characters' }),
+   message: z.string().trim().min(10, { error: 'Minimum 10 characters' }).max(400, { error: 'Maximum 400 characters' }),
 })
 
 export type ContactFormSchemaProps = z.infer<typeof contactFormSchema>
